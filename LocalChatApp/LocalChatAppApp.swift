01cea0bdd2bc285c9697b3a210db449d6db3d0c2
darkCavalier11 <sumit.kumar@changepay.in>
2024-05-17 16:33:03 +0530

    Initial Commit

---
+++ LocalChatApp/LocalChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalChatAppApp.swift
+//  LocalChatApp
+//
+//  Created by Sumit Pradhan on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct LocalChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
