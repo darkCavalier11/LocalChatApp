08ef8a67af0e02cc96b43d2eafe87484e0ba8d61
darkCavalier11 <sumit.kumar@changepay.in>
2024-05-17 21:18:37 +0530

    initial commit

--- LocalChatApp/ContentView.swift
+++ LocalChatApp/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Section {
+                } header: {
+                    Text("Connected")
+                }
+                Section {
+                } header: {
+                    Text("On my network")
+                }
+            }
+            .navigationTitle("Local Chat")
         }
-        .padding()
     }
 }
 
